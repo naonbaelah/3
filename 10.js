10e293df464e81da8b0587d65e06d2e01a60dcb0
ren <165698276+naonbaelah@users.noreply.github.com>
2024-05-18 21:51:26 +0700

    Add files via upload

---
+++ 10.js
@@ -0,0 +1,8 @@
+const listicon = document.getElementById("listicon")
+const ul = document.getElementById("ul")
+
+
+listicon.addEventListener("click", () => {
+    ul.classList.toggle("hidden");
+});
+
